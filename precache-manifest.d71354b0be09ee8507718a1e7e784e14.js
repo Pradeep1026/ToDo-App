b293ec85906a1b3dce74a1c464eb3758c9962984
Pradeep <guptapradeep1026@gmail.com>
2019-03-14 18:15:54 +0530

    Updates

---
+++ precache-manifest.d71354b0be09ee8507718a1e7e784e14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fac106da4a3eb81b0e35",
+    "url": "/to-do-app/static/js/runtime~main.fac106da.js"
+  },
+  {
+    "revision": "2cff9c5f5b4c6bce798e",
+    "url": "/to-do-app/static/js/main.2cff9c5f.chunk.js"
+  },
+  {
+    "revision": "23508a2d5402f5145dbd",
+    "url": "/to-do-app/static/js/2.23508a2d.chunk.js"
+  },
+  {
+    "revision": "2cff9c5f5b4c6bce798e",
+    "url": "/to-do-app/static/css/main.a62dc4d6.chunk.css"
+  },
+  {
+    "revision": "684fbb21612ae140f17ab790f42fdda7",
+    "url": "/to-do-app/index.html"
+  }
+];
